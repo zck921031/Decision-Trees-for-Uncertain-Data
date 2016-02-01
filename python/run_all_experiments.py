138eac343078742581aa461ef17cb68998f91038
zck921031 <zck921031@qq.com>
2016-02-01 17:48:16 +0800

    读入10个实验的数据

---
+++ python/run_all_experiments.py
@@ -0,0 +1,139 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 01 14:26:51 2016
+
+@author: zck
+"""
+
+from sklearn import cross_validation
+from sklearn import svm, tree
+import sklearn.datasets
+import pandas
+import numpy as np
+import time
+
+def mapy(y_t):
+    h = {}
+    cnt = 0
+    for t in set(y_t):
+        h[t] = cnt
+        cnt += 1
+    N = len(y_t)
+    y = []
+    for i in xrange(N):
+        y.append(h[y_t[i]])
+    return np.asarray(y, dtype=int)
+    
+def load_Banknote():
+    t = pandas.read_csv("../datasets/Banknote/data_banknote_authentication.txt",
+                        header=None).as_matrix()
+    x = t[:, 0:-1]
+    y = t[:, -1].astype(int)
+    return (x,y)
+
+def load_Bench():
+    t = pandas.read_csv("../datasets/connectionist-bench/vowel-context.data",
+                        header=None,
+                        delimiter=' ', skipinitialspace=True).as_matrix()
+    x = t[:, 3:13]
+    y = t[:, 13].astype(int)
+    return (x,y)
+
+def load_Glass():
+    t = pandas.read_csv("../datasets/glass/glass.data",
+                        header=None).as_matrix()
+    x = t[:, 1:10]
+    y = mapy( t[:, 10].astype(int) )
+    return (x, y)
+
+def load_Heart():
+    t = pandas.read_csv("../datasets/Heart/Heart.dat",
+                        header=None,
+                        delimiter=' ', skipinitialspace=True).as_matrix()
+    x = t[:, 0:13]
+    y = t[:, 13].astype(int)
+    return (x,y)
+
+def load_Iris():
+    iris = sklearn.datasets.load_iris()
+    return  (iris.data, iris.target)
+
+def load_Leaf():
+    t = pandas.read_csv("../datasets/Leaf/leaf.csv",
+                        header=None).as_matrix()
+    x = t[:, 2:]
+    y = mapy(t[:, 0].astype(int))        
+    return (x,y)
+    
+def load_Seeds():
+    t = pandas.read_csv("../datasets/Seeds/seeds.csv",
+                        header=None).as_matrix()
+    x = t[:, :-1]
+    y = mapy(t[:, -1].astype(int))
+    return (x,y)
+    
+def load_Wine():
+    t = pandas.read_csv("../datasets/wine/wine.data",
+                        header=None).as_matrix()
+    x = t[:, 1:]
+    y = mapy(t[:, 0].astype(int))
+    return (x,y)
+
+def load_Winequality():
+    t1 = pandas.read_csv("../datasets/Wine-quality/winequality-red.csv",
+                        sep=';').as_matrix()
+    t2 = pandas.read_csv("../datasets/Wine-quality/winequality-white.csv",
+                        sep=';').as_matrix()
+    t = np.vstack((t1,t2))               
+    x = t[:, :-1]
+    y = mapy(t[:, -1].astype(int))
+    return (x,y)
+    
+def load_Yeast():
+    t = pandas.read_csv("../datasets/yeast/yeast.data",
+                        header=None,
+                        delimiter=' ', skipinitialspace=True).as_matrix()
+    x = t[:,1:9]
+    y = mapy(t[:,9])
+    return (x,y)
+    
+
+def run_experiment(experiment_name, x, y, clf):
+    print( "{0}: {1}, {2}, {3}".format(experiment_name,
+              x.shape[0],x.shape[1],len(set(y))) )
+#    print( x[0] )    
+    starttime = time.clock()
+    
+    scores =cross_validation.cross_val_score(clf, x, y, cv=10)
+    
+    endtime = time.clock()
+    print scores
+    print ( 'mean is {0}, std is {1}'.format(scores.mean(),scores.std() ))
+    print("used time {0} seconds".format( endtime-starttime ) )
+    print("----------------------------------------------")
+    return scores
+    
+if __name__ == '__main__':
+    names = ['Banknote', 'Bench', 'Glass', 'Heart', 'Iris',
+             'Leaf', 'Seeds', 'Wine', 'Winequality', 'Yeast']
+    load_datas = {'Banknote':load_Banknote,
+            'Bench':load_Bench,
+            'Glass':load_Glass,
+            'Heart':load_Heart,
+            'Iris':load_Iris,
+            'Leaf':load_Leaf,
+            'Seeds':load_Seeds,
+            'Wine':load_Wine, 
+            'Winequality':load_Winequality,
+            'Yeast':load_Yeast}
+    scores = []
+    for name in names:
+        if not load_datas.has_key(name):
+            raise( name + ' loading function doesn\'t implement!' )
+        (x,y) = load_datas[name]()
+        #res = run_experiment(name, x, y, svm.SVC(kernel='linear',max_iter=5000) )
+        res = run_experiment(name, x, y, tree.DecisionTreeClassifier() )
+        scores.append(res)
+    scores = np.asarray(scores)
+
+    
